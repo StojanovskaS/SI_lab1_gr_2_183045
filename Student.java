529f7c3684a3ec3befda95bedc4b64f82e3a8a23
Sandra Stojanovska <sandrastojanovska99@yahoo.com>
2020-04-06 18:18:13 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -6,15 +6,62 @@ class Student {
 	String lastName;
 	List<Integer>grades=new ArrayList<>();
 
-	//TODO constructor
+    public Student(String index, String firstName, String lastName, List<Integer> grades) {
+        this.index = index;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grades = grades;
+    }
 
-	//TODO seters & getters
+    public String getIndex() {
+        return index;
+    }
 
-	public double getAverage() {
-		//TODO
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public int ECTSCredits() {
-		//TODO
-	}
+    public String getLastName() {
+        return lastName;
+    }
+
+    public List<Integer> getGrades() {
+        return grades;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setGrades(List<Integer> grades) {
+        this.grades = grades;
+    }
+
+    public double getAverage() {
+        double prosek=0;
+        for(int i=0;i<this.grades.size();i++){
+            prosek+=this.grades.get(i);
+        }
+        prosek/=this.grades.size();
+        return prosek;
+    }
+
+    public int ECTSCredits() {
+        int polozeni=0;
+        for (int i=0;i<this.grades.size();i++){
+            if (this.grades.get(i)>=6){
+                polozeni++;
+            }
+        }
+        polozeni*=6;
+        return polozeni;
+    }
 }
